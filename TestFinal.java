93ffd644f4a725915b582912c2b6b7d08ef6b8af
liyamariam92 <132338681+liyamariam92@users.noreply.github.com>
2023-05-21 21:20:10 +0530

    adding files

---
+++ TestFinal.java
@@ -0,0 +1,21 @@
+ class TestFinal
+ {
+int a=2;
+int b=3;
+int c;
+ public static void main (String args[]){
+	 TestFinal obj=new TestFinal();
+	 
+		 obj.sum();
+		 
+	 
+
+ }
+ public int sum()
+ {
+  c=a+b;
+  System.out.println(c);
+  return c;
+ //System.out.println(c);
+ }
+ }
